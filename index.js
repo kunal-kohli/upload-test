9c06a7954f916af8dbbb93278e42716334a85e69
unknown <kunalapptunix@apptunix.com>
2022-12-21 16:05:07 +0530

    logs removed

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const port = process.env.PORT || 9000;
 
 
 app.get('/', (req, res) => {
-    console.log('',process.env.PORT)
+    console.log('', process.env.PORT);
     res.send('WORKING');
 });
 
 app.get('/data', (req, res) => {
-    console.log('',process.env.PORT)
+    console.log('', process.env.PORT);
+    res.send('WORKING-fine');
+});
+
+app.get('/data-data', (req, res) => {
+    console.log('', process.env.PORT);
     res.send('WORKING-fine');
 });
 
